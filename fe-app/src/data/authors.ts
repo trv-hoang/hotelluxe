59eb632ba74c5744399e0efe2a88f6488f859ccb
khadon <nguyendinhdongkha@gmail.com>
2025-09-11 18:48:19 +0700

    update(json) user author image badge like

---
+++ fe-app/src/data/authors.ts
@@ -0,0 +1,52 @@
+import __authors from './jsons/__users.json';
+import avatar1 from '@/assets/avatars/image-1.jpg';
+import avatar2 from '@/assets/avatars/image-2.jpg';
+import avatar3 from '@/assets/avatars/image-3.jpg';
+import avatar4 from '@/assets/avatars/image-4.jpg';
+import avatar5 from '@/assets/avatars/image-5.jpg';
+import avatar6 from '@/assets/avatars/image-6.jpg';
+import avatar7 from '@/assets/avatars/image-7.jpg';
+import avatar8 from '@/assets/avatars/image-8.jpg';
+import avatar9 from '@/assets/avatars/image-9.jpg';
+import avatar10 from '@/assets/avatars/image-10.jpg';
+import avatar11 from '@/assets/avatars/image-11.jpg';
+import avatar12 from '@/assets/avatars/image-12.jpg';
+import avatar13 from '@/assets/avatars/image-13.jpg';
+import avatar14 from '@/assets/avatars/image-14.jpg';
+import avatar15 from '@/assets/avatars/image-15.jpg';
+import avatar16 from '@/assets/avatars/image-16.jpg';
+import avatar17 from '@/assets/avatars/image-17.jpg';
+import avatar18 from '@/assets/avatars/image-18.jpg';
+import avatar19 from '@/assets/avatars/image-19.jpg';
+import avatar20 from '@/assets/avatars/image-20.jpg';
+import type { AuthorType } from '@/types/stay';
+const imgs = [
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatar5,
+    avatar6,
+    avatar7,
+    avatar8,
+    avatar9,
+    avatar10,
+    avatar11,
+    avatar12,
+    avatar13,
+    avatar14,
+    avatar15,
+    avatar16,
+    avatar17,
+    avatar18,
+    avatar19,
+    avatar20,
+];
+
+const DEMO_AUTHORS: AuthorType[] = __authors.map((item, index) => ({
+    ...item,
+    avatar: imgs[index] || item.avatar,
+    href: item.href,
+}));
+
+export { DEMO_AUTHORS };
